4de5daddf79b5fe2b164703ac15fae4dd51756a1
Keshav-Shanbhag <49165051+Keshav-Shanbhag@users.noreply.github.com>
2021-08-13 00:18:26 +0530

    Add files via upload

---
+++ Day4/Activity4/StaticInner.java
@@ -0,0 +1,13 @@
+class StaticInner{  
+  static int data=30;  
+  static class Inner{  
+   void msg()
+   {
+	   //int data=40;
+	   System.out.println("data is "+data);}  
+  }  
+  public static void main(String args[]){  
+  StaticInner.Inner obj=new StaticInner.Inner();  
+  obj.msg();  
+  }  
+}  
