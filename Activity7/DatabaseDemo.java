2d8e0e703dfd3321615cf3dc883b12c90e397835
Keshav-Shanbhag <49165051+Keshav-Shanbhag@users.noreply.github.com>
2021-08-17 20:42:31 +0530

    Update DatabaseDemo.java

--- Activity7/DatabaseDemo.java
+++ Activity7/DatabaseDemo.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class DatabaseDemo {
   public static void main(String[] args) throws SQLException
   {
-	  Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root", "Suviksha1997@");
+	  Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root", "root");
 	  Statement s=con.createStatement();
 	  Scanner scan=new Scanner(System.in);
 	  int ch;
